2809fb6242f1e986b92829360a8bc698e88f84a1
Subrata Mahato <104990409+2004subrata@users.noreply.github.com>
2026-04-18 17:58:15 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS

---
+++ frontend/src/components/LeftSidebar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const LeftSidebar = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <img
+            width={"28px"}
+            className=""
+            src="https://img.freepik.com/free-vector/twitter-new-2023-x-logo-white-background-vector_1017-45422.jpg?t=st=1776514638~exp=1776518238~hmac=1d0f2e15b0652f1a06d04361795b03f65f33fd86f05c4be67b1510dc37aeb653&w=740"
+            alt="twitter-logo"
+          />
+        </div>
+        <div>
+            <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LeftSidebar;
