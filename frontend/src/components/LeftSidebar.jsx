010c3b6f5edd526ea60c54ab8e059e6e4c8f729a
Subrata Mahato <104990409+2004subrata@users.noreply.github.com>
2026-04-19 21:11:22 +0530

    feat: enhance LeftSidebar with navigation icons and styling

--- frontend/src/components/LeftSidebar.jsx
+++ frontend/src/components/LeftSidebar.jsx
@@ -1,19 +1,64 @@
 import React from "react";
+import { CiHome } from "react-icons/ci";
+import { CiHashtag } from "react-icons/ci";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { CiUser } from "react-icons/ci";
+import { CiBookmark } from "react-icons/ci";
+import { IoIosLogOut } from "react-icons/io";
+
 
 const LeftSidebar = () => {
   return (
-    <div>
+    <div className="w-[20%]">
       <div>
         <div>
           <img
-            width={"28px"}
-            className=""
+            width={"48px"}
+            className="ml-2"
             src="https://img.freepik.com/free-vector/twitter-new-2023-x-logo-white-background-vector_1017-45422.jpg?t=st=1776514638~exp=1776518238~hmac=1d0f2e15b0652f1a06d04361795b03f65f33fd86f05c4be67b1510dc37aeb653&w=740"
             alt="twitter-logo"
           />
         </div>
-        <div>
-            <div></div>
+        <div className="my-4">
+          <div className="flex items-center my-2 px-4 py-2 hover:bg-gray-200 hover:cursor-pointer rounded-full">
+            <div>
+              <CiHome size={"24px"} />
+            </div>
+            <h1 className="font-bold text-lg ml-2">Home</h1>
+          </div>
+          <div className="flex items-center my-2 px-4 py-2 hover:bg-gray-200 hover:cursor-pointer rounded-full">
+            <div>
+              <CiHashtag size={"24px"} />
+            </div>
+            <h1 className="font-bold text-lg ml-2">Explore</h1>
+          </div>
+          <div className="flex items-center my-2 px-4 py-2 hover:bg-gray-200 hover:cursor-pointer rounded-full">
+            <div>
+              <IoMdNotificationsOutline size={"24px"} />
+            </div>
+            <h1 className="font-bold text-lg ml-2">Notification</h1>
+          </div>
+          <div className="flex items-center my-2 px-4 py-2 hover:bg-gray-200 hover:cursor-pointer rounded-full">
+            <div>
+              <CiUser size={"24px"} />
+            </div>
+            <h1 className="font-bold text-lg ml-2">Profile</h1>
+          </div>
+          <div className="flex items-center my-2 px-4 py-2 hover:bg-gray-200 hover:cursor-pointer rounded-full">
+            <div>
+              <CiBookmark size={"24px"} />
+            </div>
+            <h1 className="font-bold text-lg ml-2">Bookmarks</h1>
+          </div>
+          <div className="flex items-center my-2 px-4 py-2 hover:bg-gray-200 hover:cursor-pointer rounded-full">
+            <div>
+              <IoIosLogOut size={"24px"} />
+            </div>
+            <h1 className="font-bold text-lg ml-2">Logout</h1>
+          </div>
+          <button className="px-4 py-2 border-none text-md bg-[#1A8CD8] w-full text-white rounded-full font-bold">
+            Post
+          </button>
         </div>
       </div>
     </div>
