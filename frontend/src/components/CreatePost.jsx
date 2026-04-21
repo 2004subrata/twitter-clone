3ab9200d4cd48daf45a10547baaf6e6d4707b298
Subrata Mahato <104990409+2004subrata@users.noreply.github.com>
2026-04-21 18:46:22 +0530

    feat: improve layout and styling of CreatePost component

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Avatar from "react-avatar";
+import { CiImageOn } from "react-icons/ci";
 
 const CreatePost = () => {
   return (
-    <div className="w-[100%]">
+    <div className="w-full">
       <div className="">
         <div className="flex items-center justify-evenly border-b border-gray-200">
           <div className="cursor-pointer hover:bg-gray-200 w-full text-center px-4 py-3">
@@ -13,8 +14,8 @@ const CreatePost = () => {
             <h1 className="font-semibold text-gray-600 text-lg">Following</h1>
           </div>
         </div>
-        <div className="m-4">
-          <div className="flex items-center">
+        <div>
+          <div className="flex items-center p-4">
             <div>
               <Avatar
                 src="https://pbs.twimg.com/profile_images/1791364266190086144/dro7T6ca_400x400.jpg"
@@ -28,8 +29,13 @@ const CreatePost = () => {
               placeholder="What is happening?!"
             />
           </div>
-          <div>
-            <button className="bg-[#1A8CD8] px-4 py-1 text-lg text-white text-right border-none rounded-full">Post</button>
+          <div className="flex items-center justify-between p-4 border-b border-gray-300">
+            <div>
+              <CiImageOn />
+            </div>
+            <button className="bg-[#1A8CD8] px-4 py-1 text-lg text-white text-right border-none rounded-full">
+              Post
+            </button>
           </div>
         </div>
       </div>
