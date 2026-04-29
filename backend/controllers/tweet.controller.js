fe63120bad61cd641821d2abc82a22c432121fdc
Subrata Mahato <104990409+2004subrata@users.noreply.github.com>
2026-04-29 23:41:10 +0530

    fix: correct user ID assignment in tweet creation and add delete route

--- backend/controllers/tweet.controller.js
+++ backend/controllers/tweet.controller.js
@@ -2,7 +2,8 @@ import { Tweet } from "../models/tweet.model.js";
 
 export const createTweetController = async (req, res) => {
   try {
-    const { description, id } = req.body;
+    const { description } = req.body;
+    const { userId } = req.user;
     if (!description || !id) {
       return res.status(401).json({
         success: false,
@@ -12,12 +13,25 @@ export const createTweetController = async (req, res) => {
 
     await Tweet.create({
       description,
-      userId: id,
+      userId: userId,
     });
 
     return res.status(201).json({
       success: true,
       message: "Tweet created successfully",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteTweet = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Tweet.findByIdAndDelete(id);
+    return res.status(200).json({
+      success: true,
+      message: "Tweet deleted successfully",
     });
   } catch (error) {
     console.log(error);
--- backend/routes/tweet.route.js
+++ backend/routes/tweet.route.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { createTweetController } from "../controllers/tweet.controller.js";
+import {
+  createTweetController,
+  deleteTweet,
+} from "../controllers/tweet.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.route("/create").post(authMiddleware, createTweetController);
+
+router.route("/delete/:id").delete(deleteTweet);
 export default router;
