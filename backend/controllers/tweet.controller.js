229e6acb3e03e0b628d2d7189c7589e8d11b7878
Subrata Mahato <104990409+2004subrata@users.noreply.github.com>
2026-04-30 22:35:43 +0530

    feat: implement like/dislike functionality and enforce auth middleware on delete route

--- backend/controllers/tweet.controller.js
+++ backend/controllers/tweet.controller.js
@@ -1,10 +1,14 @@
 import { Tweet } from "../models/tweet.model.js";
 
+/**
+ * - Create a new tweet.
+ * -  Requires an authenticated user (via authMiddleware).
+ */
 export const createTweetController = async (req, res) => {
   try {
     const { description } = req.body;
     const { userId } = req.user;
-    if (!description || !id) {
+    if (!description || !userId) {
       return res.status(401).json({
         success: false,
         message: "Feilds are required",
@@ -25,6 +29,11 @@ export const createTweetController = async (req, res) => {
   }
 };
 
+/**
+ * - Delete a tweet.
+ * - Requires an authenticated user (via authMiddleware).
+ * - Only the owner of the tweet is allowed to delete it.
+ */
 export const deleteTweet = async (req, res) => {
   try {
     const { id } = req.params;
@@ -33,6 +42,39 @@ export const deleteTweet = async (req, res) => {
       success: true,
       message: "Tweet deleted successfully",
     });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+/**
+ * like or unlike (toggle) a tweet.
+ * - If the authenticated user has already liked the tweet,
+ * their like is removed (dislike/unlike). Otherwise, the tweet is liked.
+ */
+export const likeOrDislike = async (req, res) => {
+  try {
+    const loggedInUserId = req.user.userId;
+    const tweetId = req.params.id;
+
+    const tweet = await Tweet.findById(tweetId);
+    if (tweet.like.includes(loggedInUserId)) {
+      // dislike
+      await Tweet.findByIdAndUpdate(tweetId, {
+        $pull: { like: loggedInUserId },
+      });
+      return res.status(200).json({
+        message: "User disliked you tweet",
+      });
+    } else {
+      // like
+      await Tweet.findByIdAndUpdate(tweetId, {
+        $push: { like: loggedInUserId },
+      });
+      return res.status(200).json({
+        message: "User liked you tweet",
+      });
+    }
   } catch (error) {
     console.log(error);
   }
--- backend/routes/tweet.route.js
+++ backend/routes/tweet.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createTweetController,
   deleteTweet,
+  likeOrDislike,
 } from "../controllers/tweet.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 
@@ -9,5 +10,7 @@ const router = express.Router();
 
 router.route("/create").post(authMiddleware, createTweetController);
 
-router.route("/delete/:id").delete(deleteTweet);
+router.route("/delete/:id").delete(authMiddleware, deleteTweet);
+
+router.route("/like/:id").put(authMiddleware, likeOrDislike);
 export default router;
